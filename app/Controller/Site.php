89e9ddad6f83efa30e71eafbb6f17e11b926b088
RayDe003 <barchugova03@bk.ru>
2024-04-11 17:30:40 +0700

    емае 2 день, без верстки и бд

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,19 +2,57 @@
 
 namespace Controller;
 
+use Illuminate\Database\Capsule\Manager as DB;
+use Model\Post;
 use Src\View;
+use Src\Request;
+use Model\User;
+use Src\Auth\Auth;
 
 class Site
 {
-    public function index(): string
+    public function index(Request $request): string
     {
-        $view = new View();
-        return $view->render('site.hello', ['message' => 'index working']);
+        $posts = Post::where('id', $request->id)->get();
+        return (new View())->render('site.post', ['posts' => $posts]);
     }
+
 
     public function hello(): string
     {
-        return new View('site.hello', ['message' => 'hello working']);
+        return new View('site.hello', ['message' => 'привет боба хихи']);
     }
 
+    public function signup(Request $request): string
+    {
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
+        }
+        return new View('site.signup');
+    }
+
+    public function go(Request $request): string
+    {
+        return new View('site.go', ['message' => 'привет лох']);
+    }
+
+    public function login(Request $request): string
+    {
+        //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/go');
+        }
+        //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/login');
+    }
 }
